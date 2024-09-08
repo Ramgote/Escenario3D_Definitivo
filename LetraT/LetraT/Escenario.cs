3a5ebde72f468b10e3e81ea566da8677f956c3f0
ramiro <ramiro.gonzalest@gmail.com>
2024-09-08 15:29:50 -0400

    Tarea # 2 : OpenTk Estructura Basica Implementación de la estructura básica del proyecto con las siguientes clases : (Escenario, Objeto, Partes, Poligono y Punto, y la Interface IDibujable)

---
+++ LetraT/LetraT/Escenario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using OpenTK;
+using System.Collections.Generic;
+
+namespace LetraT
+{
+    public class Escenario : IDibujable
+    {
+        public Dictionary<String, Objeto> listaDeObjetos;
+        protected Punto centro;
+
+        public Escenario(Punto centro)
+        {
+            this.listaDeObjetos = new Dictionary<String, Objeto>();
+            this.centro = centro;
+        }
+
+        public void addObjeto(String nombre, Objeto nuevoObjeto)
+        {
+            this.listaDeObjetos.Add(nombre, nuevoObjeto);
+        }
+
+        public void deleteObjeto(String objetoAEliminar)
+        {
+            this.listaDeObjetos.Remove(objetoAEliminar);
+        }
+
+        public Objeto getObjeto(String nombre)
+        {
+            return this.listaDeObjetos[nombre];
+        }
+
+        public void setObjeto(String nombre, Objeto objeto)
+        {
+            this.listaDeObjetos[nombre] = objeto;
+        }
+
+        public void Dibujar(Punto centro)
+        {
+            foreach (Objeto objetoActual in this.listaDeObjetos.Values)
+            {
+                objetoActual.Dibujar(centro);
+            }
+        }
+    }
+}
---
+++ LetraT/LetraT/Punto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetraT
+{
+    public class Punto
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Punto(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Punto(Punto punto)
+        {
+            this.x = punto.x;
+            this.y = punto.y;
+            this.z = punto.z;
+        }
+
+        public static Punto operator +(Punto p1, Punto p2) 
+        {
+            return new Punto(p1.x+p2.x, p1.y+p2.y, p1.z+p2.z);        
+        }
+    }
+}
